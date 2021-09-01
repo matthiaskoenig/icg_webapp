1d0f30e7917eb5c8ce39921770e8bfe329c8eb34
Adrian <adriankl39@googlemail.com>
2021-09-01 15:09:48 +0200

    update classification

--- classification.py
+++ classification.py
@@ -1,19 +1,41 @@
 """
-- Copy trainings data here `classificaiton_data.tsv
-- Only fit the optimal classifier for PBPK1
-- Use the classifier to predict the data
-    # predict test data
-
-    y_pred = classifier.predict(X)
-    y_score = classifier.predict_proba(X)[:, 1]
-
 - Vary resection rate & create boxplot survival ~ resection rate;
 """
+
 import numpy as np
 import pandas as pd
+from pathlib import Path
+from sklearn.svm import SVC
+
+base_path = Path(__file__).parent
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
+
+
+def classification(samples: pd.DataFrame):
+    df = pd.read_csv(
+        base_path / "data" / "classification_dataset.tsv",
+        sep="\t",
+        index_col=False
+    )
+
+    df.nonsurvival = df.nonsurvival.astype(bool)
+    X = np.array([df["postop_r15_model"]]).T
+    y = np.array(df["nonsurvival"])
+    classifier = SVC(kernel='poly', degree=2, probability=True,
+                     class_weight='balanced', random_state=42)
+
+    # fit classification model
+    classifier.fit(X=X, y=y)
+
+    # predict data
+    predict_X = np.array([samples["postop_r15_model"]]).T
+    y_pred = classifier.predict(predict_X)
+    y_score = classifier.predict_proba(predict_X)[:, 1]
+    print(y_pred)
+    print(y_score)
+
 
 if __name__ == "__main__":
     from sampling import samples_for_individual
@@ -24,5 +46,7 @@ if __name__ == "__main__":
         n=15,
     )
 
-    samples["postop_r15_model"] = np.linspace(0, 1, num=len(samples)) # dummy normal distribution
+    samples["postop_r15_model"] = np.linspace(0, 1, num=len(samples))  # dummy normal distribution
     print(samples)
+
+    classification(samples=samples)
