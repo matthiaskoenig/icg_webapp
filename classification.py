fcdf84c21b8b11bff3b6589932b2041527dab915
Matthias Koenig <konigmatt@googlemail.com>
2021-09-02 12:38:57 +0200

    updated data structure

--- classification.py
+++ classification.py
@@ -32,39 +32,55 @@ def fit_classifier():
     return classifier
 
 
-def classification(classifier, samples: pd.DataFrame) -> Tuple:
-    """Classify samples.
+def simulation(samples: pd.DataFrame, resection_rates: np.ndarray) -> pd.DataFrame:
+    """Performs model simulations and calculate pharmacokinetic parameters."""
 
-    Returns tuple of predicted probabilities and outcomes.
-    """
-    # predict data
+    dfs = []
+    for _, rate in enumerate(resection_rates):
+        df = samples.copy()
+        # simulate
+
+        # calculate pharmacokinetic parameters
+
+        # FIXME replace with simulation
+        df["resection_rate"] = rate
+        df["postop_r15_model"] = np.linspace(rate, rate + 0.09, num=len(df))
+        dfs.append(df)
+
+    return pd.concat(dfs)
+
+
+def classification(classifier, samples: pd.DataFrame) -> pd.DataFrame:
+    """Classify samples."""
     predict_X = np.array([samples["postop_r15_model"]]).T
-    y_pred = classifier.predict(predict_X)
-    y_score = classifier.predict_proba(predict_X)[:, 1]
+    samples["y_pred"] = classifier.predict(predict_X)
+    samples["y_score"] = classifier.predict_proba(predict_X)[:, 1]
 
-    # print(y_pred)
-    # print(y_score)
-    return y_score, y_pred
+    return samples
 
 
-def figure(predictions, resection_rates):
+def figure(predictions):
     """Boxplots for predictions."""
 
-    # FIXME: use actual resection rates
-    f = plt.figure(figsize=(len(predictions), 6))
-    gs = GridSpec(1, len(predictions))
+    resection_rates = sorted(predictions.resection_rate.unique())
+    n_rates = len(resection_rates)
+
+    f = plt.figure(figsize=(n_rates, 6))
+    gs = GridSpec(1, n_rates)
     gs.update(wspace=0, hspace=0)
-    for k, prediction in enumerate(predictions):
+
+    for k, rate in enumerate(resection_rates):
+        df = predictions[predictions.resection_rate == rate]
         ax = f.add_subplot(gs[0:1, k:k + 1])
-        ax = boxplot(ax, prediction, k, resection_rates[k])
+        _ = boxplot(ax, df.y_score, k, rate, n_rates)
 
     plt.show()
     return f
 
 
-def boxplot(ax, data, k, resection_rate):
+def boxplot(ax, data, k, rate, n_rates):
     ax.tick_params(axis="x", labelsize=9)
-    if k != 9:
+    if k != (n_rates-1):
         ax.spines['right'].set_visible(False)
     if k != 0:
         ax.spines['left'].set_visible(False)
@@ -74,23 +90,9 @@ def boxplot(ax, data, k, resection_rate):
 
     ax.set_ylim(top=1, bottom=0)
 
-    ax.boxplot(data, labels=[np.round(resection_rate, decimals=1)], widths=0.9, showmeans=True)
+    ax.boxplot(data, labels=[np.round(rate, decimals=1)], widths=0.9, showmeans=True)
     return ax
 
-
-def classification_for_resection_rates(classifier, samples, resection_rates = np.linspace(0, 0.9, num=10)):
-    """Classifies samples for various resection rates."""
-    predictions = []
-
-    # iterate over resection rates
-    for k in resection_rates:
-        # manual fixes: FIXME replace with simulation
-        samples["postop_r15_model"] = np.linspace(k, k + 0.09, num=len(samples))  # dummy icg_r15
-
-        y_score, y_pred = classification(classifier=classifier, samples=samples)
-        predictions.append(y_score)
-
-    return predictions
 
 classifier = fit_classifier()
 
@@ -99,16 +101,25 @@ if __name__ == "__main__":
     pd.set_option('display.max_columns', None)
     from sampling import samples_for_individual
 
+    resection_rates = np.linspace(0.1, 0.9, num=9)
     samples = samples_for_individual(
         bodyweight=75,
         age=55,
         n=15,
     )
-    resection_rates = np.linspace(0, 0.9, num=10)
-    predictions = classification_for_resection_rates(
+    print("-" * 80)
+    print(samples.head())
+
+    samples = simulation(samples, resection_rates)
+    print("-" * 80)
+    print(samples.head())
+
+    samples = classification(
         classifier=classifier,
         samples=samples,
-        resection_rates=resection_rates
     )
+    print("-" * 80)
+    print(samples.head())
+
     # figure boxplots
-    figure(predictions, resection_rates)
+    figure(samples)
